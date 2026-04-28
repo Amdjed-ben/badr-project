3790623c0e503c3d118b4a7556b87f11e2ed5ff4
Amdjed-ben <amdjedbendjebbar10@gmail.com>
2026-04-28 18:52:27 +0200

    fix pictures probleme

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 
@@ -8,5 +8,13 @@ urlpatterns = [
     path('projects/all/', views.list_projects, name='list-projects'),
     path('projects/list3/',views.list_3projects, name='list 3 projects'),
     path('categories/',views.list_category, name='list categories')
+]
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+import re
 
+# أضف هذا بعد urlpatterns الحالية
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
