9bb42347377dc91faf09a342da8a421a1caba148
Amdjed-ben <amdjedbendjebbar10@gmail.com>
2026-04-28 17:10:16 +0200

    last inshalla

--- backend/settings.py
+++ backend/settings.py
@@ -118,8 +118,9 @@ import socket
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
+        conn_health_checks=True,
     )
 }
 
