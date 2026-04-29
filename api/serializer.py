abfbe54480de2ee8acffd521d1e5d2b7897655d0
Amdjed-ben <amdjedbendjebbar10@gmail.com>
2026-04-29 20:31:53 +0200

    fix photo 5

--- api/serializer.py
+++ api/serializer.py
@@ -15,20 +15,20 @@ class Project_Serializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True}
         }
-        # def get_photo_url(self,obj):
-        #     req = self.context.get('request')
-        #     photo_url = obj.fingerprint.url
-        #     return req.build_absolute_url()
+        def get_photo_url(self,obj):
+            req = self.context.get('request')
+            photo_url = obj.fingerprint.url
+            return req.build_absolute_url()
     # def get_image(self, obj):
     #     if obj.image:
     #         return obj.image.url
     #     return None
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        if instance.image:
-            # هذا السطر يضمن تحويل المسار النسبي إلى رابط كامل
-            representation['image'] = self.context['request'].build_absolute_uri(instance.image.url)
-        return representation
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
+    #     if instance.image:
+    #         # هذا السطر يضمن تحويل المسار النسبي إلى رابط كامل
+    #         representation['image'] = self.context['request'].build_absolute_uri(instance.image.url)
+    #     return representation
 class Category_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Categore
--- api/urls.py
+++ api/urls.py
@@ -9,12 +9,3 @@ urlpatterns = [
     path('projects/list3/',views.list_3projects, name='list 3 projects'),
     path('categories/',views.list_category, name='list categories')
 ]
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
-import re
-
-# أضف هذا بعد urlpatterns الحالية
-urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-]
