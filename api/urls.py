8142b665147a26c6b071b7523865d2acd92e234b
Amdjed-ben <amdjedbendjebbar10@gmail.com>
2026-04-29 21:05:57 +0200

    no 2

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,re_path
 from . import views
 from django.conf import settings 
-
+from django.conf.urls.static import static
 urlpatterns = [
     path('client/add/', views.add_client, name='add client' ),
     path('projects/auth/', views.authenticate_project , name='authenticate project' ),
