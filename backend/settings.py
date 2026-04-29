a5804a13055df6e82b27996b40b1996c1cffc470
Amdjed-ben <amdjedbendjebbar10@gmail.com>
2026-04-29 20:37:15 +0200

    fix photos 6

--- backend/settings.py
+++ backend/settings.py
@@ -168,10 +168,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # settings.py
 
-if not DEBUG:  # تأكد أن هذا يعمل فقط في الـ Production
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
     
-    CLOUDINARY_STORAGE = {
+CLOUDINARY_STORAGE = {
         'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
         'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
         'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
